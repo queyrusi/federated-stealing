cbdaa926133206adff6b3234c7338b3ca94b983f
Simon Queyrut <38203266+queyrusi@users.noreply.github.com>
2023-09-03 20:28:03 +0200

    Improved hyperparam for FL

--- run_exp.py
+++ run_exp.py
@@ -23,12 +23,12 @@ def get_args():
 
 def get_steal_args():
     args = argparse.ArgumentParser()
-    args.add_argument('--batch_size', type=int, default=64)
+    args.add_argument('--batch_size', type=int, default=16)
     args.add_argument('--z_dim', type=int, default=128)
     args.add_argument('--lr', type=float, default=0.0002)
 
-    args.add_argument('--n_epoch', type=int, default=10)
-    args.add_argument('--n_batch', type=int, default=40)
+    args.add_argument('--n_epoch', type=int, default=30)
+    args.add_argument('--n_batch', type=int, default=10)
     args.add_argument('--s_type', type=str, default='Prob', help='Prob or Imgs, for Bayes target')
 
     args.add_argument('--s_layers', type=int, default=2, help='0, 1, 2, last layer outputs for steal CGAN')
